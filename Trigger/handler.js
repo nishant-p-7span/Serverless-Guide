b6e87d91f8d795d03d6feb0715974c9241f7f666
Nishant Parmar <160576245+nishant-p-7span@users.noreply.github.com>
2024-05-07 12:26:11 +0530

    Add files via upload

---
+++ Trigger/handler.js
@@ -0,0 +1,16 @@
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3()
+
+module.exports.readS3File = async (event) => {
+  const Key = event.Records[0].s3.object.key;
+
+  // Read the object from S3
+  const data = await s3.getObject({
+      Bucket: event.Records[0].s3.bucket.name,
+      Key
+  }).promise();
+
+  console.log("s3Object:", Key);
+  
+  return;
+};
---
+++ Upload/index.js
@@ -0,0 +1,25 @@
+const { S3Client,GetObjectCommand,PutObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+
+const s3client = new S3Client({
+    region: "ap-south-1",
+    credentials: {
+        accessKeyId: "xxxxxxx",
+        secretAccessKey: "xxxxxxxxx",
+    },
+});
+
+async function putObject(filename, contenttype) {
+    const command = new PutObjectCommand({
+        Bucket: 'xxxxxxxx',
+        Key: `uploads/${filename}`,
+        ContentType: contenttype,
+    })
+    const url = await getSignedUrl(s3client, command, { expiresIn: 60 });
+    return url;
+}
+
+exports.handler = async (event) => {
+    const url = await putObject(`image-${Date.now()}.png`, "image/png");
+    return url;
+}
